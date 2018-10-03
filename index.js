0cd807983f75bcd22dced4f60a348b0540f5da48
devinkapelanski <devinkapelanski@yahoo.com>
2018-10-03 06:41:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  var deep = document.getElementById("grand-node").querySelectorAll("div");
-  var child;
-  for (var i = 0; i < deep.length; i++) {
+  var node = document.getElementById("grand-node").querySelectorAll("div");
+  var deep;
+  for (var i = 0; i < node.length; i++) {
     
   }
 }
