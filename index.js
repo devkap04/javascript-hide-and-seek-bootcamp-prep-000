ceceb1c6e2b11bad7a900edf64bdaeb02e265d4a
devinkapelanski <devinkapelanski@yahoo.com>
2018-10-03 05:55:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  var rankedLists = document.querySelectorAll('ul.ranked-list li');
+  var rankedLists = document.querySelectorAll("ul.ranked-list li");
   for (var i = 0; i < rankedLists.length; i++); {
-    rankedLists[i].innerHTML = parseInt(rankedLists[i].innerHTML) + n;
+    rankedLists[i].innerHTML = parseInt(rankedLists[i]) + n;
   }
 }
