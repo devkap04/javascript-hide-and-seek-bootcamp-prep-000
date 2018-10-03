f33578b34e32ff60f70faa109c3853ad6cfc6a96
devinkapelanski <devinkapelanski@yahoo.com>
2018-10-03 06:54:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector) {
+    return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector("#nested .target");
+}
+
+function increaseRankBy(n) {
+  var rankedLists = document.querySelectorAll(".ranked-list li");
+  for (var i = 0; i < rankedLists.length; i++) {
+    rankedLists[i].innerHTML = parseInt(rankedLists[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  return document.querySelector("#grand-node div div div div");
+}
